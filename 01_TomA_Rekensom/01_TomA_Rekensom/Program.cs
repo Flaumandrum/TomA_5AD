48ed42565e1770785484530e3d9f8221d9541269
tomadriaens <tomadriaens@hotmail.com>
2025-09-19 09:29:22 +0200

    [Les] Analyse en commentaar

---
+++ 01_TomA_Rekensom/01_TomA_Rekensom/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace _01_TomA_Rekensom
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tom Adriaens
+            // 19/09/2025
+            // Project : Rekensom
+
+            // Velden
+
+            // Programma
+            // Stap 1: Vraag het eerste getal +opslaan
+            // Stap 2: Vraag het tweede getal +opslaan
+            // Stap 3: Maak de som + opslaan
+            // Stap 4: Vermeerder met 5 + opslaan
+            // Stap 5: Vermenigvuldig met 10 + opslaan
+            // Stap 6: Deel door 2 + opslaan
+            // Stap 7: Toon de tekst in de juiste vorm
+
+
+        }
+    }
+}
