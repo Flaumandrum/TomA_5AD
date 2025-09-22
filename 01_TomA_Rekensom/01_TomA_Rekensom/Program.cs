aca9edac241be87459218504f099a57066280af7
tomadriaens <tomadriaens@hotmail.com>
2025-09-22 10:03:54 +0200

    [Les] met tryCatch

--- 01_TomA_Rekensom/01_TomA_Rekensom/Program.cs
+++ 01_TomA_Rekensom/01_TomA_Rekensom/Program.cs
@@ -28,38 +28,53 @@ namespace _01_TomA_Rekensom
             const int _vermenigvuldiging = 10;
             const int _deling = 2;
 
-            // Programma
-            // Stap 1: Vraag het eerste getal +opslaan
-            Console.Write("Geef een getal: ");
-            _eersteGetal =int.Parse(Console.ReadLine());
+            // Programma.
+            try
+            {
 
-            // Stap 2: Vraag het tweede getal +opslaan
-            Console.Write("Geef nog een getal: ");
-            _tweedeGetal = int.Parse(Console.ReadLine());
+                // Stap 1: Vraag het eerste getal +opslaan
+                Console.Write("Geef een getal: ");
+                _eersteGetal = int.Parse(Console.ReadLine());
+                try
+                {
+                    // Stap 2: Vraag het tweede getal +opslaan
+                    Console.Write("Geef nog een getal: ");
+                _tweedeGetal = int.Parse(Console.ReadLine());
 
-            // Stap 3: Maak de som + opslaan
-            _som = _eersteGetal + _tweedeGetal;
+                // Stap 3: Maak de som + opslaan
+                _som = _eersteGetal + _tweedeGetal;
 
-            // Stap 4: Vermeerder met 5 + opslaan
-            _vermeerderd = _som + _vermeerdering;
+                // Stap 4: Vermeerder met 5 + opslaan
+                _vermeerderd = _som + _vermeerdering;
 
-            // Stap 5: Vermenigvuldig met 10 + opslaan
-            _vermenigvuldigd = _vermeerderd * _vermenigvuldiging;
+                // Stap 5: Vermenigvuldig met 10 + opslaan
+                _vermenigvuldigd = _vermeerderd * _vermenigvuldiging;
 
-            // Stap 6: Deel door 2 + opslaan
-            _gedeeld = _vermenigvuldigd / _deling;
+                // Stap 6: Deel door 2 + opslaan
+                _gedeeld = _vermenigvuldigd / _deling;
 
-            // Scherm leegmaken
-            Console.Clear();
+                // Scherm leegmaken
+                Console.Clear();
 
-            // Stap 7: Toon de tekst in de juiste vorm
-            Console.WriteLine($"U gaf het getal {_eersteGetal.ToString()}en getal {_tweedeGetal.ToString()}  in.");
-            Console.WriteLine($"De som hiervan is {_som.ToString()}");
-            Console.WriteLine($"Dit getal werd vermeerder met 5.Dit gaf als uitkomst {_vermeerderd.ToString()}." +
-                $"\nDaarna werd er vermenigvuldig met 10.Dit gaf als uitkomst {_vermenigvuldigd.ToString()}");
-            Console.WriteLine($"Als laatste werd er gedeeld door 2.");
-            Console.WriteLine($"Het uiteindelijke resultaat is {_gedeeld.ToString()}.");
+                // Stap 7: Toon de tekst in de juiste vorm
+                Console.WriteLine($"U gaf het getal {_eersteGetal.ToString()} en getal {_tweedeGetal.ToString()} in.");
+                Console.WriteLine($"De som hiervan is {_som.ToString()}");
+                Console.WriteLine($"Dit getal werd vermeerder met 5.Dit gaf als uitkomst {_vermeerderd.ToString()}." +
+                    $"\nDaarna werd er vermenigvuldig met 10.Dit gaf als uitkomst {_vermenigvuldigd.ToString()}");
+                Console.WriteLine($"Als laatste werd er gedeeld door 2.");
+                Console.WriteLine($"Het uiteindelijke resultaat is {_gedeeld.ToString()}.");
+                }
+                catch
+                {
+                    Console.WriteLine("Uw tweede geteal is niet juist.");
 
+                }
+            }
+            catch
+            { 
+                Console.WriteLine("Uw eerste geteal is niet juist.");
+
+            }
 
         }
     }
