3a0f20a4dfdf246cc4edfd26c1d37f2d159ce282
tomadriaens <tomadriaens@hotmail.com>
2025-09-26 09:57:33 +0200

    [LES] oefening 16 afgewerkt

---
+++ 02_TomA_Kleur/02_TomA_Kleur/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _02_TomA_Kleur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tom Adriaens 
+            // 26/09/2025
+            // Oef 16: Project Kleur
+
+            // Velden 
+            Byte _keuze = 0;
+
+            // Programma 
+            // Stap 1: Intro
+            Console.WriteLine("Welkom bij het kleurprogramma!");
+            Console.WriteLine("\nDruk op een toets om verder te gaan...");
+            Console.ReadKey();
+
+            // Scherm leegmaken
+            Console.Clear();
+
+            // Stap 2: Toon keuzemenu(Rood, groen, blauw)
+            Console.WriteLine("Kies een kleur uit het menu:");
+            Console.WriteLine("  1. Rood \n  2. Groen \n  3. Blauw");
+
+            // Foutafhandeling
+            try
+            {
+
+                // Stap 3: vraag keuze +opslaan
+                Console.Write("\nUw keuze: ");
+                _keuze = Byte.Parse(Console.ReadLine());
+
+
+                // Scherm leegmaken
+                Console.Clear();
+
+                // Stap 4: 
+                // •	Als keuze Rood
+                if (_keuze == 1)
+                {
+                    // o   Toon Tekst
+                    Console.WriteLine("U koos voor de kleur rood!");
+                }
+
+                // •	Als keuze groen
+                else if (_keuze == 2)
+                {
+                    // o   Toon Tekst
+                    Console.WriteLine("U koos voor de kleur groen!");
+                }
+
+                // •	Als keuze blauw
+                else if (_keuze == 3)
+                {
+                    // o   Toon Tekst
+                    Console.WriteLine("U koos voor de kleur blauw!");
+
+                }
+                else
+                {
+                    // o   foutmelding
+                    Console.WriteLine("U gaf geen geldige keuze in!");
+
+                }
+                Console.WriteLine("\nDruk op een toets om verder te gaan...");
+                Console.ReadKey();
+            }
+            catch
+            {   
+                // Scherm leegmaken
+                Console.Clear();
+
+                // Foutmelding
+                Console.WriteLine("U gaf geen juist getal in!");
+                Console.WriteLine("\nDruk op een toets om verder te gaan...");
+                Console.ReadKey();
+
+                
+            }
+
+        }
+    }
+}
---
+++ 02_TomA_Kleur/02_TomA_Kleur/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("02_TomA_Kleur")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("02_TomA_Kleur")]
+[assembly: AssemblyCopyright("Copyright © HP 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6cf50a41-7cbe-418b-977c-9855287e4321")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
