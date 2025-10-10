1aa9f525eb6867b1de0a3ea2d1a28ce34f6ecd48
tomadriaens <tomadriaens@hotmail.com>
2025-10-10 10:45:24 +0200

    [LES] start oef random

---
+++ 06_TomA_Start/06_TomA_Start/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_TomA_Start
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Tom Adriaensz
+            // 10/10/2025
+            // Project Start
+
+            // velden 
+
+            // Programma
+
+            // Zeg hallo
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
