83b686d31bbc4a0c4aba3dbff5f03bcc6823f86a
tomadriaens <tomadriaens@hotmail.com>
2025-10-16 13:54:54 +0200

    [LES] afgewerkt

--- 07_TomA_WillekeurigGetal/07_TomA_WillekeurigGetal/Program.cs
+++ 07_TomA_WillekeurigGetal/07_TomA_WillekeurigGetal/Program.cs
@@ -10,7 +10,23 @@ namespace _07_TomA_WillekeurigGetal
     {
         static void Main(string[] args)
         {
-            // Tom 
+            // Tom Adriaens
+            // 10/10/2025
+            // Willekeurig getal
+
+            // Velden 
+            Random _rdm1 = new Random();
+            
+
+            int _getal1 = 0;
+            int _getal2 = 0;
+
+            // Programma
+            _getal1 = _rdm1.Next(5,11);
+            _getal2 = _rdm1.Next(1,20);
+
+            Console.WriteLine($"Eerste getal:  {_getal1.ToString()}");
+            Console.WriteLine($"Tweede getal:  {_getal2.ToString()}");
         }
     }
 }
