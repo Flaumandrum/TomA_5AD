ce9641ac0e33b552747ece3174cae5a2d78a6a2a
tomadriaens <tomadriaens@hotmail.com>
2026-01-13 15:23:09 +0100

    [LES] arrays oef 61 auto vullen array

---
+++ 16_TomA_GegArray1/16_TomA_GegArray1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;,
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_TomA_GegArray1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tom Adriaens
+            // 13/01/2026
+            // Project: Gegevens array 1
+
+            // Velden 
+            Byte[] _getallen = new Byte[10];
+            byte _getal = 1;
+            // Programma 
+            //Stap 1: Vul de array
+            for (int i= 0; i < _getallen.Count(); i ++)
+            {
+                _getallen[i] = (byte)(i+1);
+                
+            }
+            
+            //Stap 2: laat de gegevens zien dmv een do lus
+
+            //Stap 3: Laat de gegevens zien dmv een for lus
+
+
+        }
+    }
+}
