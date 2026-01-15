590e2607377954bbf0718ac5e52e13c192d4a6b1
tomadriaens <tomadriaens@hotmail.com>
2026-01-15 14:09:09 +0100

    [Les] Afgewerkt

--- 16_TomA_GegArray1/16_TomA_GegArray1/Program.cs
+++ 16_TomA_GegArray1/16_TomA_GegArray1/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;,
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,7 +16,7 @@ namespace _16_TomA_GegArray1
 
             // Velden 
             Byte[] _getallen = new Byte[10];
-            byte _getal = 1;
+            
             // Programma 
             //Stap 1: Vul de array
             for (int i= 0; i < _getallen.Count(); i ++)
@@ -24,11 +24,36 @@ namespace _16_TomA_GegArray1
                 _getallen[i] = (byte)(i+1);
                 
             }
-            
+
             //Stap 2: laat de gegevens zien dmv een do lus
+            int t = 0;
+            do
+            {
+                if(t== (_getallen.Count()-1))
+                {
+                    Console.WriteLine($"{_getallen[t]} ");
+                }
+                else
+                {
+                    Console.Write($"{_getallen[t]}, ");
+                }
+                    
+
+                t++;
+            } while (t < _getallen.Count());
 
             //Stap 3: Laat de gegevens zien dmv een for lus
-
+            for(int i = 0; i< _getallen.Count();i++)
+            {
+                if (i == (_getallen.Count() - 1))
+                {
+                    Console.WriteLine($"{_getallen[i]} ");
+                }
+                else
+                {
+                    Console.Write($"{_getallen[i]}, ");
+                }
+            }
 
         }
     }
