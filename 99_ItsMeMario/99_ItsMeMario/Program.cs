aad1a72db3ca82deae701765a3b5b8f2b80a1edf
tomadriaens <tomadriaens@hotmail.com>
2026-02-09 08:41:04 +0100

    [Les] functies start

---
+++ 99_ItsMeMario/99_ItsMeMario/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _99_ItsMeMario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> noten = new Dictionary<string, int>();
+
+            noten["Do"] = 262;
+            noten["Dok"] = 262;
+            noten["Re"] = 262;
+            noten["Rek"] = 262;
+            noten["Mi"] = 262;
+            noten["Fa"] = 262;
+            noten["Fak"] = 262;
+            noten["Sol"] = 262;
+            noten["Solk"] = 262;
+            noten["La"] = 262;
+            noten["Lak"] = 262;
+            noten["Si"] = 262;
+            noten["Do"] = 262;
+
+
+
+        }
+    }
+}
