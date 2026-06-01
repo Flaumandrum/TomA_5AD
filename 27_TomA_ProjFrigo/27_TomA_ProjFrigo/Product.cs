ce09d28b21b8a55f74b3ea2f490fed8de1f61b8c
tomadriaens <tomadriaens@hotmail.com>
2026-06-01 08:51:42 +0200

    [LES] klasse toegevoegd

---
+++ 27_TomA_ProjFrigo/27_TomA_ProjFrigo/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_TomA_ProjFrigo
+{
+    internal class Product
+    {
+        // Velden + properties
+        String _naam { get; set; }
+        DateTime _vervaldatum { get; set; }
+
+        
+
+        // Functies 
+
+
+        // Constructors
+
+
+    }
+}
